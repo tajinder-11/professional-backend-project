742f22c6d3ba0550b63e03e939fc5874038726d9
tajinder-11 <dheertajinder@gmail.com>
2024-08-19 17:56:25 +0530

    chore: done basic configurations

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,20 @@
-const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error)
-    );
-  };
+// const asyncHandler = (requestHandler) => {
+//   (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+//       next(error)
+//     );
+//   };
+// };
+
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
 
 export { asyncHandler };
-
-// const asynsHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req, res, next);
-//   } catch {
-//     res.status(err.code || 500).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-// };
